941db9e8cd2946a0d539a0ca475f9853f41bc4c4
Tony Gaitatzis <tonyg@backupbrain.org>
2014-09-13 10:15:46 -0700

    updating router software

--- www/assets/js/router.js
+++ www/assets/js/router.js
@@ -156,7 +156,7 @@ function updateStatus() {
 			num_clients = accesspoint_clients.length;
 			$("#accesspoint_client_number_value").text(num_clients);
 
-			$('#access_point_clients > tbody').html("");
+			$('#access_point_clients tbody').empty();
 			for (index in accesspoint_clients) {
 				mac = accesspoint_clients[index].mac;
 				ip = accesspoint_clients[index].ip;
@@ -165,7 +165,7 @@ function updateStatus() {
 				row = $("<tr/>");
 				row.append(mac_cell);
 				row.append(ip_cell);
-				$('#access_point_clients > tbody').append(row);
+				$('#access_point_clients tbody').append(row);
 			}
 			$("#access_point_clients").show();
 			
