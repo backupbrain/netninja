87607ea2e8c81af4102da34d0155ed7253b990e2
Tony Gaitatzis <tonyg@backupbrain.org>
2014-09-13 10:01:55 -0700

    updating router software

--- www/application/status.php
+++ www/application/status.php
@@ -123,10 +123,10 @@ foreach (array_keys($internal_interfaces) as $interface) {
 
 $accesspoint_clients = array();
 $raw_connection_text = rtrim(`sudo ../../scripts/accesspoint/connections.sh --interface=wlan1`,"\n");
-$raw_connection_list = explode($raw_connection_text, "\n");
+$raw_connection_list = explode("\n", $raw_connection_text);
 print_r($raw_connection_list);
 foreach ($raw_connection_list as $raw_connection_pair) {
-	$raw_connection = explode($raw_connection_pair, " ");
+	$raw_connection = explode(" ", $raw_connection_pair);
 	$macaddress = $raw_connection[0];
 	$ipaddress = $raw_connection[1];
 	$accesspoint_clients[$macaddress] = $ipaddress;
