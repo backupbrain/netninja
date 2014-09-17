a491a44ccde174ff5a615c45c13027f081abd019
Tony Gaitatzis <tonyg@backupbrain.org>
2014-09-16 17:33:58 -0700

    updating router software

--- scripts/wifi_client/get_ssid.sh
+++ scripts/wifi_client/get_ssid.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 config_file="/etc/network/interfaces"
-config_setting="wpa-ssid"
+wpa_config_setting="wpa-ssid"
+wep_config_setting="wireless-essid"
 
 
-config_result=`sed -n '/^[[:space:]]'$config_setting' "\(.*\)"$/s//\1/p' < $config_file`
+config_result=`sed -n '/^[[:space:]]'$wpa_config_setting' "\(.*\)"$/s//\1/p' < $config_file`
 
 
 if [[ ! -z "$config_result" ]];then
-	config_result=`sed -n '/^[[:space:]]'$config_setting' "\(.*\)"$/s//\1/p' < $config_file`
+	config_result=`sed -n '/^[[:space:]]'$wep_config_setting' "\(.*\)"$/s//\1/p' < $config_file`
 fi
 
 echo $config_result
