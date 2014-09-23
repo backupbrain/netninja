96ce69277c9b8f6977510f6f192d99144849e6e1
Tony Gaitatzis <tonyg@backupbrain.org>
2014-09-22 20:02:24 -0700

    updating router software

--- scripts/adblock/enable.sh
+++ scripts/adblock/enable.sh
@@ -8,7 +8,7 @@ source_script=$cwd/$update_script
 cron_script=/etc/cron.weekly/$update_script
 
 if [ ! -f $cron_script ]; then
-	ln -s $source_script $cron_script
+	ln -sf $source_script $cron_script
 fi
 
 $source_script
