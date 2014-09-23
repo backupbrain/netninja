3cbded8baf67a15666e544812bd23d935a1b6d41
Tony Gaitatzis <tonyg@backupbrain.org>
2014-09-22 19:51:05 -0700

    updating router software

--- scripts/adblock/enable.sh
+++ scripts/adblock/enable.sh
@@ -2,6 +2,7 @@
 
 update_script=update-adblock.sh
 cwd=`DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
+echo cwd
 source_script=$cwd/$update_script
 
 cron_script=/etc/cron.weekly/$update_script
